d361fdf1277722d7dd0a740672b602a21822bd7b
guojiashuang <guojiashuang@58.com>
2023-01-12 13:34:38 +0800

    支持xcframework

--- lib/cocoapods-util/hooks/pod_target.rb
+++ lib/cocoapods-util/hooks/pod_target.rb
@@ -1,58 +1,41 @@
 module Pod
-    class PodTarget < Target
-        def enable_framework_header_search_path
-            Pod::Podfile::DSL.enable_targets.include? self.name
-        end
-
-        # 获取file_accessors中vendored_frameworks的header_search_paths
-        def accessors_framework_header_paths(target)
-            header_search_paths = []
-            target.file_accessors.each do |file_accessor|
-                file_accessor.vendored_frameworks.select do |path| 
-                    header_dir_path = Pathname.new("#{path}/Headers")
-                    next unless header_dir_path.exist?
-                    relative_path_to_sandbox = header_dir_path.relative_path_from(target.sandbox.root)
-                    header_search_paths.push "${PODS_ROOT}/#{relative_path_to_sandbox}"
-                end
-            end
-            header_search_paths
-        end
-
-        # 获取target依赖子target的framework_header_search_paths，解决使用framework时无法使用`""`引用头文件的问题
-        def framework_header_search_path(configuration: nil, from_aggregate_target: false)
-            # switch enable
-            return [] unless self.enable_framework_header_search_path
-
-            framework_header_search_paths = []
-            # aggregate_target
-            if from_aggregate_target
-                framework_header_search_paths.concat(accessors_framework_header_paths(self)) 
-            end
-            return framework_header_search_paths unless should_build? 
-            
-            # should_build 
-            dependent_targets = recursive_dependent_targets(:configuration => configuration)
-            dependent_targets.each do |target|
-                framework_header_search_paths.concat(accessors_framework_header_paths(target))
-            end
-            framework_header_search_paths
-        end
-    end
-end
-
-module Pod
     class Target
       # @since 1.5.0
       class BuildSettings
+        def enable_missing_framework_header(pt)
+            Pod::Podfile::DSL.enable_targets.include? pt.name
+        end
+
+        def missing_framework_header_search_path(pt)
+            return [] unless enable_missing_framework_header(pt)
+
+            paths = []
+            pt.file_accessors.each do |file_accessor|
+                file_accessor.vendored_xcframeworks.map { |path| 
+                    Xcode::XCFramework.new(file_accessor.spec.name, path) 
+                }.each { |xcfwk| 
+                    xcfwk.slices.each { |slice|
+                        name = slice.path.basename
+                        paths.push "${PODS_XCFRAMEWORKS_BUILD_DIR}/#{xcfwk.target_name}/#{name}/Headers" unless name.nil?
+                    }
+                }
+
+                (file_accessor.vendored_frameworks - file_accessor.vendored_xcframeworks).each { |framework|
+                    header_path = Pathname.new("#{framework}/Headers")
+                    next unless header_path.exist?
+                    paths.push "${PODS_ROOT}/#{header_path.relative_path_from(pt.sandbox.root)}"
+                }
+            end
+            paths.uniq
+        end
+
          # A subclass that generates build settings for a `PodTarget`
          class AggregateTargetSettings
             # @return [Array<String>]
             alias_method :old_raw_header_search_paths, :_raw_header_search_paths
             def _raw_header_search_paths
                 header_search_paths = old_raw_header_search_paths
-                target.pod_targets.each do |t|
-                    header_search_paths.concat t.framework_header_search_path :configuration => @configuration, :from_aggregate_target => true
-                end
+                header_search_paths.concat pod_targets.flat_map { |pt| missing_framework_header_search_path(pt) }
                 header_search_paths.uniq
             end
          end
@@ -63,7 +46,8 @@ module Pod
             alias_method :old_raw_header_search_paths, :_raw_header_search_paths
             def _raw_header_search_paths
                 header_search_paths = old_raw_header_search_paths
-                header_search_paths.concat target.framework_header_search_path :configuration => @configuration
+                # header_search_paths.concat missing_framework_header_search_path(target)
+                header_search_paths.concat dependent_targets.flat_map { |pt| missing_framework_header_search_path(pt) } if target.should_build?
                 header_search_paths.uniq
             end
          end
